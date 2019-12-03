9c45976b46e1e2fddbe2f4ba59524a260bbb13fd
zurika777 <44270254+zurika777@users.noreply.github.com>
2019-12-03 21:28:45 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>დამზადებულია</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <!--[if lte IE 9]> <link rel="stylesheet" href="css/stylesie9.css"><![endif]-->
+</head>
+<body>
+<section class="advantages">
+    <div class="advItems">
+        <div class="advItem advItem-left">
+            <div class="advItem__box"><img src="img/adv-1.png" alt="">
+                <div class="advItem__title">ragac satauri</div>
+                <dic class="advItem__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magn.
+                </dic>
+            </div>
+        </div>
+        <div class="advItem advItem-middle">
+            <div class="advItem__box"><img src="img/adv-2.png" alt="" class="advItem__icon advItem__icon-small">
+                <div class="advItem__title">ragac satauri</div>
+                <dic class="advItem__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magn.
+                </dic>
+            </div>
+        </div>
+        <div class="advItem advItem-right">
+            <div class="advItem__box"><img src="img/adv-3.png" alt="" class="advItem__icon ">
+                <div class="advItem__title">ragac satauri</div>
+                <dic class="advItem__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magn.
+                </dic>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="blog">
+    <div class="wrapper">
+        <h2 class="blogTitle">From the blog</h2>
+        <div class="blogItems">
+            <div class="blogItem">
+                <img src="img/blog-1.jpg" alt="" class="blogItem_img" class="advItem__icon">
+                <div class="blogItem__title">+1 step on the career ladder</div>
+                <div class="blogItem__text">The 20 th course otf the up-grade program has just finished and 87 participants from 49 countries...
+                </div>
+                <div class="blogItem__meta">
+                    <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-user" >
+                            <span
+                            class="some">Supe user</span>
+                        </span>
+                        <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-calendar" >
+                            <span
+                                class="some">06 April 2019</span>
+                        </span>
+
+                </div>
+            </div>
+            <div class="blogItem">
+                <img src="img/blog-2.jpg" alt="" class="blogItem_img">
+                <div class="blogItem__title">Our new home</div>
+                <div class="blogItem__text">Since Septemberr 3 we have been waiting for our Ukrainian guests. Our new office was openes in ...
+                </div>
+                <div class="blogItem__meta">
+                    <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-user" >
+                            <span
+                                class="some">Supe user</span>
+                        </span>
+                        <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-calendar" >
+                            <span
+                                class="some">06 April 2019</span>
+                        </span>
+
+                </div>
+            </div>
+            <div class="blogItem">
+                <img src="img/blog-3.jpg" alt="" class="blogItem_img">
+                <div class="blogItem__title">Fall business-school</div>
+                <div class="blogItem__text">This year fall company business-school will join ambitious, purposeful people in New York. You can ...
+                </div>
+                <div class="blogItem__meta">
+                    <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-user" >
+                            <span
+                                class="some">Supe user</span>
+                        </span>
+                        <span class="blogItem__metaItem">
+                        <span class="blogItem__metaIcon fa fa-calendar" >
+                            <span
+                                class="some">06 April 2019</span>
+                        </span>
+
+                </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="blogButtons">
+            <div class="btn btn-green">See all post</div>
+        </div>
+    </div>
+</section>
+<section class="stats">
+    <div class="wrapper">
+        <div class="statItems">
+            <div class="statItem">
+                <div class="statItem__icon"><img src="img/stat-icon-1.png" alt=""></div>
+                <div class="statItem__number">258</div>
+                <div class="statItem__text">Elit do eiusmod tempo</div>
+            </div>
+            <div class="statItem">
+                <div class="statItem__icon"><img src="img/stat-icon-2.png" alt=""></div>
+                <div class="statItem__number">58</div>
+                <div class="statItem__text">Elit do eiusmod tempo</div>
+            </div>
+            <div class="statItem">
+                <div class="statItem__icon"><img src="img/stat-icon-3.png" alt=""></div>
+                <div class="statItem__number">1,897</div>
+                <div class="statItem__text">Elit do eiusmod tempo</div>
+            </div>
+            <div class="statItem">
+                <div class="statItem__icon"><img src="img/stat-icon-4.png" alt=""></div>
+                <div class="statItem__number">786</div>
+                <div class="statItem__text">Elit do eiusmod tempo</div>
+            </div><!-- todo მარტო ეს არაა-->
+        </div><!--todo mesec sheidzleba-->
+    </div>
+</section>
+</body>
+</html>
